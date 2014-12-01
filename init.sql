527c9d3ae3cf3053f8a6f527f37b19cb0a13f39f
efaurie <efaurie@hawk.iit.edu>
2014-12-01 16:09:13 -0600

    SQL

---
+++ init.sql
@@ -0,0 +1,40 @@
+BEGIN;
+
+CREATE TABLE access_points(
+    Mac TEXT,
+    Ssid TEXT,
+    LastSeen DATETIME
+);
+
+CREATE TABLE hound_config(
+    SetTime DATETIME,
+    Mode TEXT,
+    Args TEXT
+);
+
+INSERT INTO hound_config VALUES('2014-01-01 00:00:00.0000', 'SCAN', '');
+
+CREATE TABLE mac_addresses(
+    Mac TEXT,
+    LastSeen DATETIME,
+    State TEXT,
+    StateDelta INT
+);
+
+CREATE TABLE twitter_credentials(
+    Owner TEXT,
+    AccessTokenKey TEXT,
+    AccessTokenSecret TEXT,
+    ConsumerKey TEXT,
+    ConsumerSecret TEXT
+);
+
+INSERT INTO twitter_credentials VALUES(
+    'iit_cs549_p',
+    '2898195857-Cr7Z1yV4HmgAu3OqI5hdIssuhmo3K9ManQ8LC2b',
+    'YUeanihKiwGkRV6lPLixEyzxq9v5ugrwFjjetoapzLncC',
+    'jDHthtAE82TMhaCztUZNrNTcn',
+    'sDgbfm4OZB0xKPYpx0qQFeaFel1Xvq8aSqloMyZw40Ag9t82vv'
+);
+
+END;
